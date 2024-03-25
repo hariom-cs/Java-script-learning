4350feae7decdea7d0441032b555f7b7d7804996
hariom-cs <hariomcse01@gmail.com>
2024-03-25 16:59:18 +0530

    function-expression

--- function.js
+++ function.js
@@ -1,10 +1,19 @@
-function calculator(str, a, b){
-    if(str=='add'){
-        return function add(){
-            console.log(a+b);
-        }
-    }
+// function calculator(str, a, b){
+//     if(str=='add'){
+//         return function add(){
+//             console.log(a+b);
+//         }
+//     }
+// }
+// let anss=calculator('add', 3, 8);
+// console.log(anss);
+// anss();
+
+//------------------------------fuction  expression
+
+let sayHi=function abcd(){
+    console.log("this is a function expression");
 }
-let anss=calculator('add', 3, 8);
-console.log(anss);
-anss();
+// sayHi();//it will run
+console.log(""+sayHi()); 
+// abcd();//--> error= "fuction abcd is not defined"
