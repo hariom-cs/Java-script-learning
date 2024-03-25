a08256726534397681cf81f890ed79123d0ab726
hariom-cs <hariomcse01@gmail.com>
2024-03-25 21:31:51 +0530

    Immediately Invoked function expression

--- function.js
+++ function.js
@@ -11,9 +11,23 @@
 
 //------------------------------fuction  expression
 
-let sayHi=function abcd(){
-    console.log("this is a function expression");
-}
+// let sayHi=function abcd(){
+//     console.log("this is a function expression");
+// }
 // sayHi();//it will run
-console.log(""+sayHi()); 
+// console.log(""+sayHi); //return fun. definition
+// sayHi();//OP->this is a function expression
 // abcd();//--> error= "fuction abcd is not defined"
+
+
+
+//-----------Immediately Invoked function Expression------
+let ans1= additionIIFE=function add(a,b){
+    return a+b;
+}(122,34);
+console.log(ans1);// op= 156
+
+// eg-02
+let ans2= additionIIFE=function add(a,b){
+    console.log(a+b);
+}(122,340);//op=462
