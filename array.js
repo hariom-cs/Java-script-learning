9237f6faf56fdd962983609d2a69f84f8abaf39b
hariom-cs <hariomcse01@gmail.com>
2024-03-25 21:58:12 +0530

    array

---
+++ array.js
@@ -0,0 +1,16 @@
+let  car=["bmw", "audi", "rossroyal", 4, 5, 6, true];
+console.log(car);//[ 'bmw', 'audi', 'rossroyal', 4, 5, 6, true ]
+
+//->we can store different value of different types in js array
+//->accessing the element the array
+console.log(car[0]);//op=>bmw
+
+//->replacing element in array
+car[0]="fortuner";
+console.log(car[0]);//op=> fortuner
+
+//->adding  element in array
+car[7]="mahindra"
+console.log(car)
+
+
