f2adb51f5d0eece34d5f23fafb2f0cebd12b42a2
hariom-cs <hariomcse01@gmail.com>
2024-03-26 11:13:04 +0530

    string-mthods

--- string.js
+++ string.js
@@ -1,8 +1,35 @@
-//string--> it is a sequence of character
+// //string--> it is a sequence of character
 
 var str="Hello Prepcoder";
-console.log(str[4]);
-console.log(str);
-console.log(str.length);
+// console.log(str[4]);
+// console.log(str);
+// console.log(str.length);
 
-//slice method
+// //slice method
+// let slicedStr=str.slice(2,7)//-> Information is intact and has not been tempered with.
+// console.log(slicedStr);
+// console.log(str);
+
+// console.log("--------------------");
+// let slicedStr1=str.slice(2)
+// console.log(slicedStr1);
+// console.log(str);
+ 
+
+
+//extraction method
+let extract_str=str.substr(2,5)
+console.log(extract_str);
+
+
+//replacing string
+str="YOLO"
+console.log(str.toLowerCase());//-> Information is intact and has not been tempered with.
+console.log(str);
+console.log("----------------------------------");
+str="fomo"
+console.log(str.toUpperCase());//-> Information is intact and has not been tempered with.
+console.log(str);
+
+
+
