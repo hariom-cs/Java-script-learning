8785f74ab71fb806ef88a8422035deb3da351e4e
hariom-cs <hariomcse01@gmail.com>
2024-04-02 04:50:57 +0530

    Nested Object and Its Accessing

--- objets.js
+++ objets.js
@@ -20,3 +20,28 @@ let str="Hariom";
 console.log(str.length);//dot  notation
 console.log(str["length"]);//square bracket notation
 
+//nested objects in javascript
+let capAmerica={
+    firstName:"steve",
+    lastName:"rogers",
+    friends:["bucky", "tony stark", "burce"],
+    age:190,
+    isAvenger:true,
+
+    address:{//another object in objects
+        state:"manhattan",
+        city:"Newyork",
+        country:"USA"
+    },
+    sayHi:function(){
+        console.log(`Hello my name is ${this.firstName}`);
+    }
+};
+
+console.log(capAmerica);
+console.log(capAmerica.friends);
+console.log(capAmerica.friends[0]);
+console.log("printed using bracket notation "+  capAmerica["friends"][0]);
+capAmerica.sayHi();//methods accessing
+
+console.log(capAmerica.address.city);//nested objects
