9f12e90dd53d6bfc7b45717d765881e5a2130a48
hariom-cs <hariomcse01@gmail.com>
2024-04-09 04:33:53 +0530

    interview question till 20

---
+++ interview.js
@@ -0,0 +1,40 @@
+// const person={
+//     firstName:"Hariom",
+//     lastName:"Kumar",
+//     pet:{
+//         name:"Mara",
+//         breed:"Dutch TUlip Hound",
+//     },
+//     getFullName: function getFullName(){
+//         return `${this.firstName} ${this.lastName}`
+//     },
+// };
+
+// console.log(person.getFullName());
+
+//----------------------------
+
+ 
+// function sumValus(x, y, z){
+//     return x+y+z;
+// }
+// console.log(sumValus([...1,2,3]));//type error
+
+// console.log(sumValus(...[1,2,3]));//output->6
+
+// console.log(sumValus([...[1,2,3]]));//1,2,3undefinedundefined
+
+// console.log(sumValus([1,2,3]));//1,2,3undefinedundefined
+
+//----------------------
+// function nums(a,b){
+//     if(a>b)console.log("a is big")
+//     else console.log("b is big");
+//     return 
+//     a+b;
+// }
+
+// console.log(nums(2,3));//if return and a+b in same line then op= b is big 5
+// console.log(nums(5,4));//if return and a+b not in same line=>op= a is big undefined
+//----------------------------------------------
+
