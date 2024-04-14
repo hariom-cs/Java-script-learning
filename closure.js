6b4ad5f5032374461a1b192f7e44e1dbe6db8290
hariom-cs <hariomcse01@gmail.com>
2024-04-14 13:44:52 +0530

    closure understand

---
+++ closure.js
@@ -0,0 +1,22 @@
+// function outer(){
+//     var a =10;
+//     function inner(){
+//         console.log(a);
+//     }
+//     inner();
+// }
+// outer();
+
+
+
+function outer(){
+    var a =10;
+    function inner(){
+        console.log(a);
+    }
+    return inner;//it will return inner function with  its lexical socpe(i.e. outer fun. ki local memory)
+}
+ var z= outer();
+ console.log(z+"");
+ console.log(outer()+"");//This one  and above line are same
+ z();
