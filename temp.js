2824a1735de445de3f138d318aa5ad55ff8ba5b0
hariom-cs <hariomcse01@gmail.com>
2024-04-21 00:37:01 +0530

    require-export works

--- temp.js
+++ temp.js
@@ -7,3 +7,11 @@ var obj={
     },
     hello:1234
 }
+
+console.log(obj);
+
+module.exports = obj;//by this whole object obj is exported to fs.js file of variable "abc"
+
+//now by this 2 times print same thing 
+//1st-since require method execute the file (for line 11)
+//2nd time- since exports methods used so exported to variable "abc"(fo line 13)
